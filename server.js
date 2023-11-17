85b3d9825413a6f809ff69bb6b3a3d75790a75ad
nizarfdlh <suncodev@gmail.com>
2023-11-17 21:26:02 +0700

    run server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const cookieSession = require('cookie-session');
+const app = express();
+
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+
+// parse request of content-type - application/json
+app.use(express.json());
+
+// Listening PORT
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
+
+// Routes
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to Neraca Application' });
+});
