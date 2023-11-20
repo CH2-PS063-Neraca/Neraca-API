d7bd84e60ffcfd826efe7a28697279eff89ec5b3
nizarfdlh <suncodev@gmail.com>
2023-11-20 15:02:55 +0700

    create account, login user and admin, and logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,107 @@
-exports.allAccess = (req, res) => {
-    res.status(200).send('Public Content');
-}
+const User = require('../models/userModels');
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const { generateRefreshToken } = require('../config/refreshToken');
+const { generateToken } = require('../config/jwtToken')
 
-exports.advokatBoard = (req, res) => {
-    res.status(200).send('Advokat Content');
-}
+/* --------------- Membuat Akun --------------  */
+const createUser = asyncHandler(async(req, res) => {
+    const email = req.body.email;
+    const findUser = await User.findOne({ email: email });
 
-exports.adminBoard = (req, res) => {
-    res.status(200).send('Admin Content');
-}
+    if (!findUser) {
+        // Jika akun tidak ditemukan makan user membuat akun
+        const newUser = await User.create(req.body);
+        res.json(newUser);
+    } else {
+        // Jika akun sudah terdaftar memunculkan pesan : 'Akun sudah terdaftar'
+        throw new Error('Akun sudah terdaftar!');
+    }
+});
+
+/* -------------- Login User -------------------- */
+const signInController = asyncHandler(async(req, res) => {
+    const { email, password } = req.body;
+    // Check apakah user tersedia atau tidak
+    const findUser = await User.findOne({ email });
+    if (findUser && (await findUser.isPasswordMatched(password))) {
+        const refreshToken = await generateRefreshToken(findUser?._id);
+        const updateUser = await User.findByIdAndUpdate(
+            findUser.id,
+            {
+                refreshToken: refreshToken,
+            },
+            { new: true}
+        );
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            maxAge: 72 * 60 * 60 * 1000,
+        });
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id),
+        });
+    } else {
+        throw new Error('Invalid Credentials');
+    }
+});
+
+/* -------------- Login Admin -------------------- */
+const sigInAdminController = asyncHandler(async(req, res) => {
+    const { email, password } = req.body;
+    // Check apakah user tersedia atau tidak
+    const findAdmin = await User.findOne({ email });
+    if (findAdmin.role !== 'admin') throw new Error('Not Authorized');
+    if (findAdmin && (await findAdmin.isPasswordMatched(password))) {
+        const refreshToken = await generateRefreshToken(findAdmin?._id);
+        const updateUser = await User.findByIdAndUpdate(
+            findAdmin.id,
+            {
+                refreshToken: refreshToken,
+            },
+            { new: true }
+        );
+
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            maxAge: 72 * 60 * 60 * 1000,
+        });
+        res.json({
+            _id: findAdmin?._id,
+            firstname: findAdmin?.firstname,
+            lastname: findAdmin?.lastname,
+            email: findAdmin?.email,
+            mobile: findAdmin?.mobile,
+            token: generateToken(findAdmin?._id),
+        })
+    } else {
+        throw new Error('Invalid Credentials');
+    }
+});
+
+/* -------------- Logout Akun -------------------- */
+const logout = asyncHandler(async(req, res) => {
+    const cookie = req.cookies;
+    if (!cookie?.refreshToken) throw new Error('No refresh token in cookies');
+    const refreshToken = cookie.refreshToken;
+    const user = await User.findOne({ refreshToken });
+    if (!user) {
+        res.clearCookie('refreshToken', {
+            httpOnly: true,
+            secure: true,
+        });
+        return res.sendStatus(204);
+    }
+    await User.findOneAndUpdate(refreshToken, {
+        refreshToken: '',
+    });
+    res.clearCookie('refreshToken', {
+        httpOnly: true,
+        secure: true,
+    });
+    res.sendStatus(204);
+});
