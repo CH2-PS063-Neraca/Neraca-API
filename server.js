88d99ade5c7ac26dfc681ae06939a70436f8065f
nizarfdlh <suncodev@gmail.com>
2023-11-28 22:10:55 +0700

    add library and function

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
 const express = require('express');
-const dbConnect = require('./config/dbConnect');
-const authRouter = require('./routes/authRoute');
 const cors = require('cors');
-const app = express();
+const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
+const sequelize = require('./config/dbConfig');
+const User = require('./models/userModels');
 
+const app = express();
 require('dotenv').config();
+
+// Port Server
 const PORT = process.env.PORT || 8080;
 
-// Database Connection
-dbConnect();
+app.use(cors());
 
+// Database Connection
+async function initializeDatabase() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+
+        // Synchronize models with the database
+        await sequelize.sync({ force: true });
+        console.log('Models have been synchronize successfully.')
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+initializeDatabase();
+
+// Configuration
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 
 // Routes
-app.use('/api/user', authRouter);
+require('./routes/userRoute')(app);
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to Neraca Application' });
 });
+
 
 // Listening PORT
 app.listen(PORT, () => {
