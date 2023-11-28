98d2b6c24df570743a61ce44e72c32f00537d48a
nizarfdlh <suncodev@gmail.com>
2023-11-28 23:12:55 +0700

    create refresh token jwt

--- config/refreshToken.js
+++ config/refreshToken.js
@@ -1,7 +1,10 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const generateRefreshToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '3d' });
-};
+    return jwt.sign({ id }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: '72h'
+    });
+}
 
 module.exports = { generateRefreshToken };
