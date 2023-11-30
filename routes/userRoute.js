631423c0bf36312a430efa8be8684531650362ea
nizarfdlh <suncodev@gmail.com>
2023-11-30 17:31:22 +0700

    create endpoint for authcontroller

---
+++ routes/userRoute.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const controller = require('../controllers/userController');
+
+module.exports = function(app) {
+    app.use(function(req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, Content-Type, Accept"
+        );
+        next();
+    });
+
+    app.get('/api/users', controller.getUsers);
+    app.post('/api/updatePassword', controller.updatePassword);
+}
