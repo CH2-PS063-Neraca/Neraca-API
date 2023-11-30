6408de4677b0a86d62ffeba311dc0777b7bf673c
nizarfdlh <suncodev@gmail.com>
2023-11-30 17:32:02 +0700

    create getuser controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,107 +1,77 @@
-const User = require('../models/userModels');
-const jwt = require('jsonwebtoken');
-const asyncHandler = require('express-async-handler');
-const { generateRefreshToken } = require('../config/refreshToken');
-const { generateToken } = require('../config/jwtToken')
+const { User } = require('../models/userModels');
+const bcrypt = require('bcryptjs');
 
-/* --------------- Membuat Akun --------------  */
-const createUser = asyncHandler(async(req, res) => {
-    const email = req.body.email;
-    const findUser = await User.findOne({ email: email });
 
-    if (!findUser) {
-        // Jika akun tidak ditemukan makan user membuat akun
-        const newUser = await User.create(req.body);
-        res.json(newUser);
-    } else {
-        // Jika akun sudah terdaftar memunculkan pesan : 'Akun sudah terdaftar'
-        throw new Error('Akun sudah terdaftar!');
-    }
-});
-
-/* -------------- Login User -------------------- */
-const signInController = asyncHandler(async(req, res) => {
-    const { email, password } = req.body;
-    // Check apakah user tersedia atau tidak
-    const findUser = await User.findOne({ email });
-    if (findUser && (await findUser.isPasswordMatched(password))) {
-        const refreshToken = await generateRefreshToken(findUser?._id);
-        const updateUser = await User.findByIdAndUpdate(
-            findUser.id,
-            {
-                refreshToken: refreshToken,
-            },
-            { new: true}
-        );
-        res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            maxAge: 72 * 60 * 60 * 1000,
+/* ----------- Get User ------------- */
+exports.getUsers = async (req, res) => {
+    try {
+        const users = await User.findAll({
+            attributes: ['id', 'username', 'email'],
         });
-        res.json({
-            _id: findUser?._id,
-            firstname: findUser?.firstname,
-            lastname: findUser?.lastname,
-            email: findUser?.email,
-            mobile: findUser?.mobile,
-            token: generateToken(findUser?._id),
+        res.send(users);
+    } catch (error) {
+        res.status(401).send({
+            status: 'Failed',
+            message: 'Terjadi kesalahan mendapatkan user'
         });
-    } else {
-        throw new Error('Invalid Credentials');
     }
-});
+}
+/* -------- Update Password ------- */
+exports.updatePassword = async (req, res) => {
+    try {
+        const id = req.id;
 
-/* -------------- Login Admin -------------------- */
-const sigInAdminController = asyncHandler(async(req, res) => {
-    const { email, password } = req.body;
-    // Check apakah user tersedia atau tidak
-    const findAdmin = await User.findOne({ email });
-    if (findAdmin.role !== 'admin') throw new Error('Not Authorized');
-    if (findAdmin && (await findAdmin.isPasswordMatched(password))) {
-        const refreshToken = await generateRefreshToken(findAdmin?._id);
-        const updateUser = await User.findByIdAndUpdate(
-            findAdmin.id,
+        const { password, newPassword, confPassword } = req.body;
+
+        const user = await User.findByPk(id);
+
+        if (!user) {
+            return res.status(404).send({
+                status: 'Failed',
+                message: 'User not found',
+            });
+        }
+
+        const matchPassword = await bcrypt.compare(password, user.password);
+
+        if (!matchPassword) {
+            return res.status(401).send({
+                status: 'Failed',
+                message: 'Incorrect current password, please check your password',
+            });
+        }
+
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+        const updateUser = await User.update(
+            { password: hashedPassword },
             {
-                refreshToken: refreshToken,
-            },
-            { new: true }
+                where: {
+                    id: id,
+                },
+            }
         );
 
-        res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            maxAge: 72 * 60 * 60 * 1000,
+        if (updateUser) {
+            return res.status(200).send({
+                status: 'Success',
+                message: 'Update berhasil',
+            });
+        } else {
+            return res.status(500).send({
+                status: 'Failed',
+                message: 'Update failed',
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({
+            status: 'Failed',
+            message: 'Internal server error',
         });
-        res.json({
-            _id: findAdmin?._id,
-            firstname: findAdmin?.firstname,
-            lastname: findAdmin?.lastname,
-            email: findAdmin?.email,
-            mobile: findAdmin?.mobile,
-            token: generateToken(findAdmin?._id),
-        })
-    } else {
-        throw new Error('Invalid Credentials');
     }
-});
+};
 
-/* -------------- Logout Akun -------------------- */
-const logout = asyncHandler(async(req, res) => {
-    const cookie = req.cookies;
-    if (!cookie?.refreshToken) throw new Error('No refresh token in cookies');
-    const refreshToken = cookie.refreshToken;
-    const user = await User.findOne({ refreshToken });
-    if (!user) {
-        res.clearCookie('refreshToken', {
-            httpOnly: true,
-            secure: true,
-        });
-        return res.sendStatus(204);
-    }
-    await User.findOneAndUpdate(refreshToken, {
-        refreshToken: '',
-    });
-    res.clearCookie('refreshToken', {
-        httpOnly: true,
-        secure: true,
-    });
-    res.sendStatus(204);
-});
+/* -------- Update Profile -------- */
+/* -------- Get User Profile -------- */
