d259168d6dea05cda72c767268ee97225386884e
nizarfdlh <suncodev@gmail.com>
2023-11-30 14:35:09 +0700

    create endpoint register, sign in, and sign out

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const router  = express.Router();
+const controller = require('../controllers/authController');
 
-router.post('/register')
+module.exports = function(app) {
+    app.use(function(req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, Content-Type, Accept"
+        );
+        next();
+    });
 
-module.exports = router;
+    app.post('/api/auth/register', controller.register);
+    app.post('/api/auth/login', controller.signIn);
+    app.post('/api/auth/logout', controller.signOut);
+
+}
