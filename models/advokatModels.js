31cad059594f263ea02c6ec89fb897744df32f32
nizarfdlh <suncodev@gmail.com>
2023-12-03 01:24:28 +0700

    create table db advokat models

---
+++ models/advokatModels.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/dbConfig');
+
+const Advokat = sequelize.define('advokat', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nama: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    confPassword: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    firma: {
+        type: DataTypes.STRING,
+    },
+    pengalaman: {
+        type: DataTypes.STRING,
+    },
+    keahlian: {
+        type: DataTypes.STRING,
+    },
+    biografy: {
+        type: DataTypes.STRING,
+    },
+    foto: {
+        type: DataTypes.STRING,
+    },
+    riwayat_kasus: {
+        type: DataTypes.STRING,
+    },
+    pendidikan: {
+        type: DataTypes.STRING,
+    },
+    lokasi: {
+        type: DataTypes.STRING,
+    },
+});
+
+module.exports = Advokat;
